08d71c227ff952205e40eb01bc6f54765596fb6f
marcodeangelis <marcodeangelis09@gmail.com>
2023-08-23 14:57:47 +0100

    tests were failing on sin function because of floating-point endpoints. This has been fixed using a new method called almost_contain(x,y)

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -10,7 +10,24 @@ import numpy
 from numpy import ndarray
 
 from intervals.number import Interval as I
-from intervals.methods import (intervalise,lo,hi)
+from intervals.methods import (intervalise,lo,hi,contain,almost_contain)
+from intervals.random import uniform_endpoints
+
+from intervals.methods import (abs,sqrt,exp,sin,cos)
+
+PI = numpy.pi
+numpy_sin = numpy.sin
+numpy_exp = numpy.exp
+numpy_linspace = numpy.linspace
+
+X_LO_TRIG = [-2*PI, -3/2*PI, -PI, -PI/2, 0, PI/2, PI, 3/2*PI, 2*PI]
+X_HI_TRIG = [ -3/2*PI, -PI, -PI/2, 0, PI/2, PI, 3/2*PI, 2*PI, 5/2*PI]
+
+def zeroit(x, epsilon=1e-5):
+    x = numpy.asarray(x, dtype=float)
+    z = abs(x)<epsilon
+    x[z] = 0.0
+    return x
 
 class TestMethods(unittest.TestCase):
     def test_lo(self): pass
@@ -24,12 +41,39 @@ class TestMethods(unittest.TestCase):
 class TestUnary(unittest.TestCase):
     def test_abs(self): pass
     def test_sqrt(self): pass
-    def test_exp(self): pass
+    def test_exp_1(self): 
+        x = numpy.random.rand()
+        y = exp(x)
+        y_numpy = numpy_exp(x)
+        self.assertEqual(y,y_numpy)
+    def test_exp_2(self): 
+        x = uniform_endpoints(n=1,left_bound=0,right_bound=9)
+        y = exp(x)
+        y_lo = numpy_exp(x.lo)
+        y_hi = numpy_exp(x.hi)
+        self.assertEqual(y.lo,y_lo)
+        self.assertEqual(y.hi,y_hi)
     def test_log(self): pass
 
 class TestTrig(unittest.TestCase):
-    def test_sin(self): pass
-    def test_sinvec(self): pass
+    def test_sin_1(self): 
+        xx = I(lo=X_LO_TRIG, hi=X_HI_TRIG)
+        yy = intervalise([sin(xi) for xi in xx]) 
+        x_ = numpy_linspace(min(X_LO_TRIG),max(X_HI_TRIG),num=10)
+        y_ = numpy_sin(x_)
+        for xxi,yyi in zip(xx,yy):  
+            x_in_xx_i = contain(xxi,x_)
+            y_in_yy_i = y_[x_in_xx_i]
+            self.assertEqual(all(almost_contain(yyi,y_in_yy_i, tol=1e-9)),True)
+    def test_sinvec_1(self): 
+        xx = I(lo=X_LO_TRIG, hi=X_HI_TRIG)
+        yy = sin(xx) # invokes vector sin
+        x_ = numpy_linspace(min(X_LO_TRIG),max(X_HI_TRIG),num=10)
+        y_ = zeroit(numpy_sin(x_))
+        for xxi,yyi in zip(xx,yy):  
+            x_in_xx_i = contain(xxi,x_)
+            y_in_yy_i = y_[x_in_xx_i]
+            self.assertEqual(all(almost_contain(yyi,y_in_yy_i, tol=1e-9)),True)
     def test_cos(self): pass
     def test_cosvec(self): pass
     def test_tan(self): pass
